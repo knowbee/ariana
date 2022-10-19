ecf93bdbbe80d509ac01ffcee4ef203c41845d48
knowbee <knowbeeinc@gmail.com>
2022-10-19 12:25:34 +0200

    Refactor live streaming screen

--- lib/screens/live_streaming.dart
+++ lib/screens/live_streaming.dart
@@ -1,14 +1,17 @@
+import 'package:ariana/model/app_tab.dart';
+import 'package:ariana/providers/app_tab.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 
-class VideoLiveStreaming extends StatefulWidget {
+class VideoLiveStreaming extends ConsumerStatefulWidget {
   const VideoLiveStreaming({Key? key}) : super(key: key);
 
   @override
   _VideoLiveStreamingState createState() => _VideoLiveStreamingState();
 }
 
-class _VideoLiveStreamingState extends State<VideoLiveStreaming> {
+class _VideoLiveStreamingState extends ConsumerState<VideoLiveStreaming> {
   late VideoPlayerController _controller;
 
   @override
@@ -24,31 +27,58 @@ class _VideoLiveStreamingState extends State<VideoLiveStreaming> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.0),
+    AppTabs currentTab = ref.watch(appTabProvider);
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Live Streaming'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              ref.read(appTabProvider.notifier).changeTab(0);
+              final tab = ref.read(appTabProvider);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => tab.screen,
+                ),
+              );
+              // go to homepage
+            },
+          ),
         ),
-        child: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : const Text('Loading...'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Center(
+                child: _controller.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      )
+                    : const CircularProgressIndicator(),
+              ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
         ),
       ),
     );
