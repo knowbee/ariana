d8245e2265b8af01fb6de4adad666f9abcae963f
knowbee <knowbeeinc@gmail.com>
2022-10-19 11:07:34 +0200

    Adds news details screen

--- lib/widgets/breaking_news_item.dart
+++ lib/widgets/breaking_news_item.dart
@@ -1,59 +1,82 @@
 import 'package:ariana/model/news.dart';
+import 'package:ariana/screens/news_details.dart';
 import 'package:flutter/material.dart';
 
 class BreakingNewsItem extends StatelessWidget {
-  const BreakingNewsItem({Key? key, required this.newsItem}) : super(key: key);
+  const BreakingNewsItem({
+    Key? key,
+    required this.newsItem,
+    this.height,
+    this.radius,
+    this.padding,
+    this.margin,
+  }) : super(key: key);
 
   final News newsItem;
+  final double? height;
+  final double? radius;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding:
-          const EdgeInsets.only(top: 16.0, bottom: 16, left: 24, right: 64),
-      margin: const EdgeInsets.only(right: 16),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(
-          colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.5), BlendMode.dstATop),
-          image: NetworkImage(newsItem.image),
-          fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewsDetails(newsItem: newsItem),
+          ),
+        );
+      },
+      child: Container(
+        height: height,
+        margin: margin ?? const EdgeInsets.only(right: 16),
+        padding:
+            const EdgeInsets.only(top: 16.0, bottom: 16, left: 24, right: 64),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(16),
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5), BlendMode.dstATop),
+            image: NetworkImage(newsItem.image),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              Chip(
-                label: Text(newsItem.categories[0].name),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Icon(Icons.fiber_manual_record,
-                  size: 8, color: Colors.grey.shade300),
-              const SizedBox(
-                width: 8,
-              ),
-              Text(
-                newsItem.formattedDate,
-                style: TextStyle(color: Colors.grey.shade300),
-              )
-            ],
-          ),
-          const SizedBox(height: 16),
-          Text(
-            newsItem.title,
-            style: TextStyle(
-                color: Colors.grey.shade100,
-                fontWeight: FontWeight.w600,
-                fontSize: 18),
-          ),
-        ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Chip(
+                  label: Text(newsItem.categories[0].name),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Icon(Icons.fiber_manual_record,
+                    size: 8, color: Colors.grey.shade300),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  newsItem.formattedDate,
+                  style: TextStyle(color: Colors.grey.shade300),
+                )
+              ],
+            ),
+            const SizedBox(height: 16),
+            Text(
+              newsItem.title,
+              style: TextStyle(
+                  color: Colors.grey.shade100,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/news_list_item.dart
+++ lib/widgets/news_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:ariana/model/news.dart';
+import 'package:ariana/screens/news_details.dart';
 import 'package:flutter/material.dart';
 
 class NewsListItem extends StatelessWidget {
@@ -7,62 +8,72 @@ class NewsListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 100,
-          height: 80,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade200,
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              image: NetworkImage(newsItem.image),
-              fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewsDetails(newsItem: newsItem),
+          ),
+        );
+      },
+      child: Row(
+        children: [
+          Container(
+            width: 100,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: NetworkImage(newsItem.image),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  newsItem.title,
-                  style: TextStyle(
-                    color: Colors.grey.shade900,
-                    fontWeight: FontWeight.w500,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    newsItem.title,
+                    style: TextStyle(
+                      color: Colors.grey.shade900,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      newsItem.author,
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
+                  Row(
+                    children: [
+                      Text(
+                        newsItem.author,
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 8),
-                    Icon(
-                      Icons.fiber_manual_record,
-                      size: 8,
-                      color: Colors.grey.shade400,
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      newsItem.formattedDate,
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
+                      const SizedBox(width: 8),
+                      Icon(
+                        Icons.fiber_manual_record,
+                        size: 8,
+                        color: Colors.grey.shade400,
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 8),
+                      Text(
+                        newsItem.formattedDate,
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
